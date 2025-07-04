7526345ac5137814bbc8e6003b422bd4441035e8
Quantico-Bullet <Brian.Kagiso.Lebang@gmail.com>
2025-07-04 20:46:22 +0200

    .

--- nnunet_mednext/training/network_training/nnUNetTrainer.py
+++ nnunet_mednext/training/network_training/nnUNetTrainer.py
@@ -153,7 +153,7 @@ class nnUNetTrainer(NetworkTrainer):
         wandb.login(key="51ffe1022d9cb8e7e7a504cbf9a800d732b5de57")
         run = wandb.init(
             project="MedNeXt_BDOU",
-            name = f"MedNeXt_TEST_8_BDOU_CE",
+            name = f"MedNeXt_TEST_9_BDOU_CE",
             config={                     
                 "learning_rate": self.initial_lr
             },
